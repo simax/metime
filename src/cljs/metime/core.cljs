abeb5dcad5b54917c2951eaac5f9570c4714d1cc
Simon Lomax <simax.99.99@gmail.com>
2015-07-06 22:30:10 +0100

    Separated  Secretary routes into its own file. Pull into each of the views to call route name from href's

--- src/cljs/metime/core.cljs
+++ src/cljs/metime/core.cljs
@@ -4,6 +4,7 @@
   (:require [goog.events :as events]
             [goog.history.EventType :as EventType]
             [secretary.core :as secretary :refer-macros [defroute]]
+            [metime.routes]
             [metime.navigation.views :as nv]
             [metime.navigation.subs]
             [metime.navigation.handlers]
@@ -13,43 +14,12 @@
                                    dispatch
                                    dispatch-sync
                                    subscribe]]
-            [metime.utils :as utils])
+            [metime.utils])
   (:import goog.History))
 
 
 (enable-console-print!)
 (secretary/set-config! :prefix "#")
-
-(defroute tables-route "/tables" []
-          (dispatch [:switch-route :tables :tables]))
-
-(defroute calendar-route "/calendar" []
-          (dispatch [:switch-route :calendar :calendar]))
-
-(defroute file-manager-route "/file-manager" []
-          (dispatch [:switch-route :file-manager :file-manager]))
-
-(defroute user-route "/user" []
-          (dispatch [:switch-route :user :user]))
-
-(defroute login-route "/login" []
-          (dispatch [:switch-route :login :login]))
-
-(defroute employees-route "/employees" []
-          (dispatch [:switch-route :employees :employees]))
-
-(defroute root-route "/" []
-          (utils/set-hash! "#/employees")
-          (dispatch [:switch-route :employees :employees]))
-
-(defroute employee-route "/employee/:id" [id]
-          (dispatch [:employee-route-switcher :employees :employee id]))
-
-(defroute employee-add-route "/employees/add" []
-          (dispatch [:employee-route-switcher :employees :employee 0]))
-
-(defroute "*" []
-          (dispatch [:switch-route :employees :not-found]))
 
 (defn hook-browser-navigation! []
   "Routing history, back button etc."
---
+++ src/cljs/metime/routes.cljs
@@ -0,0 +1,39 @@
+(ns metime.routes
+  (:require [metime.utils :as utils]
+            [secretary.core :refer-macros [defroute]]
+            [re-frame.core :refer [register-handler
+                                   path
+                                   dispatch
+                                   dispatch-sync
+                                   subscribe]]))
+
+(defroute tables-route "/tables" []
+          (dispatch [:switch-route :tables :tables]))
+
+(defroute calendar-route "/calendar" []
+          (dispatch [:switch-route :calendar :calendar]))
+
+(defroute file-manager-route "/file-manager" []
+          (dispatch [:switch-route :file-manager :file-manager]))
+
+(defroute user-route "/user" []
+          (dispatch [:switch-route :user :user]))
+
+(defroute login-route "/login" []
+          (dispatch [:switch-route :login :login]))
+
+(defroute employees-route "/employees" []
+          (dispatch [:switch-route :employees :employees]))
+
+(defroute root-route "/" []
+          (utils/set-hash! "#/employees")
+          (dispatch [:switch-route :employees :employees]))
+
+(defroute employee-route "/employee/:id" [id]
+          (dispatch [:employee-route-switcher :employees :employee id]))
+
+(defroute employee-add-route "/employees/add" []
+          (dispatch [:employee-route-switcher :employees :employee 0]))
+
+(defroute "*" []
+          (dispatch [:switch-route :employees :not-found]))
