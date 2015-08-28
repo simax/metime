14019b33ac28f2bede4a4ff59473b32eee163f86
Simon Lomax <simax.99.99@gmail.com>
2015-08-28 17:08:34 +0100

    Tinkering with Clojure and ClojureScript versions in project.clj in an attempt to get REPL working .

--- project.clj
+++ project.clj
@@ -4,8 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-            :dependencies [[org.clojure/clojure "1.7.0"]
+            :dependencies [
+                           [org.clojure/clojure "1.7.0"]
                            [org.clojure/clojurescript "0.0-3297"]
+
+                           ;[org.clojure/clojure "1.6.0"]
+                           ;[org.clojure/clojurescript "0.0-3211"]
+
                            [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                            [figwheel "0.3.6"]
                            [figwheel-sidecar "0.3.6"]
